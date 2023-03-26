a0c924d8d98d7e69b10d58d2df6fbfd7d7ce6cd5
Daniel <danizmd8@gmail.com>
2023-03-26 13:57:18 +0300

    [FEATURE] Added argument parser and blueprint to NBT parser class

---
+++ NBTParser.py
@@ -0,0 +1,12 @@
+import struct
+
+
+class Parser():
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+    
+    def parse(self) -> None:
+        pass
+    
+    def save_to_file(self, output_file_path: str) -> None:
+        pass
--- NBTSnake.py
+++ NBTSnake.py
@@ -1,3 +1,34 @@
 """
     A simple Minecraft NBT parser
+    
+    Specification: https://web.archive.org/web/20110723210920/http://www.minecraft.net/docs/NBT.txt
 """
+
+import argparse
+from NBTParser import Parser
+
+
+def get_args_from_user() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser(
+        prog="NBTSnake",
+        description="Minecraft NBT parser written in python"
+    )
+    
+    arg_parser.add_argument("--file", "-f", help="Input file in .nbt format", required=True)
+    arg_parser.add_argument("--output", "-o", help="Output file. JSON format", required=True)
+    
+    return arg_parser.parse_args()
+
+
+def main():
+    args = get_args_from_user()
+    
+    nbt_parser = Parser(args.file)
+
+    nbt_parser.parse()
+    
+    nbt_parser.save_to_file(args.output)
+
+
+if __name__ == '__main__':
+    main()
